9d5df1d90ef01165486db7eaa70502bd77254c91
frantzeska <saiti.frantzeska@gmail.com>
2019-01-20 14:36:36 +0200

    creating new controller from scratch

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   root 'simple_pages#index'
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 end
