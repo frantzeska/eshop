8078814db47312aaefc1d7dbff6b73dcfeb23400
frantzeska <saiti.frantzeska@gmail.com>
2019-01-25 20:22:37 +0200

    change the number of products in carousel

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,6 +4,7 @@ class SimplePagesController < ApplicationController
 
   def landing_page
     @featured_product = Product.first
-    @products = Product.limit(3)
+    #@products = Product.limit(3)
+    @products = Product.all
   end
 end
