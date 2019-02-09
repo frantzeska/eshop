aa2334d730d6e9a09781c89805f6542e8ed2c2ae
frantzeska <saiti.frantzeska@gmail.com>
2019-02-09 16:26:44 +0200

    try to delect a product from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'simple_pages#index'
   get '/products', to: 'products#index'
   get 'simple_pages/landing_page'
-  delete '/products/:5'
+  delete '/products/:5', to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :orders, only: [:index, :show, :create, :destroy]
