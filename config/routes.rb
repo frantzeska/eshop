b89b795004ea6d26312e470e2f7580c81a308567
frantzeska <saiti.frantzeska@gmail.com>
2019-02-09 15:38:19 +0200

    change the root to the route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/index'
+  root 'simple_pages/index'
   get '/products', to: 'products#index'
-  root 'simple_pages#landing_page'
+  get 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :orders, only: [:index, :show, :create, :destroy]
