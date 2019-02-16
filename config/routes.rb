cd7be69116fac2f3cd47cc7298485fca9e1519e1
frantzeska <saiti.frantzeska@gmail.com>
2019-02-16 10:09:14 +0200

    try add a contact form on contact page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get '/products', to: 'products#index'
+  post 'simple_pages/thank_you'
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
