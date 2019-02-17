8c3371877af076405c646f7a2a58c05911b7c518
frantzeska <saiti.frantzeska@gmail.com>
2019-02-17 14:26:22 +0200

    try to create authentication

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -12,7 +12,7 @@ class SimplePagesController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     ActionMailer::Base.mail(from: @email,
-      to: 'your-email@example.com',
+      to: 'frantzeska2008@hotmail.com',
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
   end
