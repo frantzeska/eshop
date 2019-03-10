8a1ca29ddfa87cc3692006d9c1416183e3fd1052
frantzeska <saiti.frantzeska@gmail.com>
2019-03-10 11:29:14 +0200

    add admin role & moderate user comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if
+      user ||= User.new # guest user (not logged in)
+      can :manage, User, id: user.id
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
+    else
+      user.admin? #administrator
+      can :manage, :all
+    end
     #   else
     #     can :read, :all
     #   end
