59550717720997e04c1a169deba3c991402a2b39
frantzeska <saiti.frantzeska@gmail.com>
2019-03-12 20:15:37 +0200

    try to fix destroy method

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if
-      user ||= User.new # guest user (not logged in)
-      can :manage, User, id: user.id
+    #if
+      #user ||= User.new # guest user (not logged in)
+      can :read, :all
+      #can :manage, User, id: user.id
+    if user.present?  # additional permissions for logged in users (they can manage their posts)
+      can :manage, Post, user_id: user.id
     # Define abilities for the passed in user here. For example:
     #
     else
