7a6be8715e2dfdc9bd6cb52e714d10b28d00b24c
frantzeska <saiti.frantzeska@gmail.com>
2019-03-20 22:21:04 +0200

    amend routes for auto reply

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
-  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}, controllers: { registrations: "user_registrations" }
+  devise_for :users, controllers: { registrations: "user_registrations" }
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :users
   resources :products do
     resources :comments
