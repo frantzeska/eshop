c4a17922ab0011a3330682532a12663af63d71a1
frantzeska <saiti.frantzeska@gmail.com>
2019-03-20 22:13:26 +0200

    fix grid system, auto email, grid system as per Giota's instructions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}, controllers: { registrations: "user_registrations" }
   resources :users
   resources :products do
     resources :comments
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages#landing_page'
   get '/products', to: 'products#index'
   post 'simple_pages/thank_you'
   root 'simple_pages#landing_page'
