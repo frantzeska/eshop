31f08d799f087441465cadb05ae61d4ebad510f7
frantzeska <saiti.frantzeska@gmail.com>
2019-03-25 01:18:40 +0200

    try to fix unit tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,19 +2,23 @@ require 'rails_helper'
 
 describe Order do
 
-    let(:product) { Product.create!(name:"book", price:100)}
+    let(:product) { Product.create!(name:"book", description:"I am a description", price:100)}
     let(:user) {User.create!(email: "test@gmail.com", password: "password")}
 
     it "is a valid order" do
-      expect(Order.new( user: user, product: product, total: 100, price:100)).to be_valid
+      expect(Order.new( user: user, product: product, total: 100, description:"I am a description", price:100)).to be_valid
     end
 
     it "is not valid without a user" do
-      expect(Order.new( user: "", product: product, total: 100, price:100)).not_to be_valid
+      expect(Order.new( user: "", product: product, total: 100, description:"I am a description", price:100)).not_to be_valid
     end
 
     it "is not valid without product" do
-      expect(Order.new( user: user, product: "", total: 100, price:100)).not_to be_valid
+      expect(Order.new( user: user, product: "", total: 100, description:"I am a description", price:100)).not_to be_valid
+    end
+
+    it "is not valid without description" do
+      expect(Order.new( user: user, product: product, total: 100, description:"", price:100)).not_to be_valid
     end
 
     it "is not valid without price" do
