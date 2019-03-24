0837a3df51aed198a82a6920c5824d45462ada58
frantzeska <saiti.frantzeska@gmail.com>
2019-03-25 01:13:27 +0200

    try to fix unit tests

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,26 +6,26 @@ describe Order do
     let(:user) {User.create!(email: "test@gmail.com", password: "password")}
 
     it "is a valid order" do
-      expect(Order.new( user: user, product: product, total: 100, name:"book", description:"I am a description", price:100)).to be_valid
+      expect(Order.new( user: user, product: product, total: 100, description:"I am a description", price:100)).to be_valid
     end
 
     it "is not valid without a user" do
-      expect(Order.new( user: "", product: product, total: 100, name:"book", description:"I am a description", price:100)).not_to be_valid
+      expect(Order.new( user: "", product: product, total: 100, description:"I am a description", price:100)).not_to be_valid
     end
 
     it "is not valid without product" do
-      expect(Order.new( user: user, product: "", total: 100, name:"book", description:"I am a description", price:100)).not_to be_valid
+      expect(Order.new( user: user, product: "", total: 100, description:"I am a description", price:100)).not_to be_valid
     end
 
     it "is not valid without name" do
-      expect(Order.new( user: user, product: product, total: 100, name:"", description:"I am a description", price:100)).not_to be_valid
+      expect(Order.new( user: user, product: product, total: 100, description:"I am a description", price:100)).not_to be_valid
     end
 
     it "is not valid without description" do
-      expect(Order.new( user: user, product: product, total: 100, name:"book", description:"", price:100)).not_to be_valid
+      expect(Order.new( user: user, product: product, total: 100, description:"", price:100)).not_to be_valid
     end
 
     it "is not valid without price" do
-      expect(Order.new( user: user, product: product, total: 100, name:"book", description:"I am a description", price:"")).not_to be_valid
+      expect(Order.new( user: user, product: product, total: 100, description:"I am a description", price:"")).not_to be_valid
     end
 end
