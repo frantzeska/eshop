a0ad9008f2cb88467376dfaf530c584370bf338f
frantzeska <saiti.frantzeska@gmail.com>
2019-03-27 20:28:59 +0200

    try to create functional tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,30 +6,31 @@ describe UsersController, type: :controller do
 
   describe 'GET #show' do
      context 'when a user is logged in' do
-       before do
-        sign_in user
+
+      before do
+        sign_in @user
       end
 
       it 'loads correct user details' do
-         get :show, params: {id: user.id}
-         expect(response).to be_ok
-         expect(assigns(:user)).to eq user
-       end
+        get :show, params: { id: @user.id }
+        expect(response).to be_ok
+        expect(assigns(:user)).to eq @user
+      end
 
-       it "it can't access the Show page of the second user" do
-         get :show, params: {id: user2.id}
-         expect(response).to have_http_status(302)
-         expect(response).to redirect_to(root_path)
-       end
 
+      it 'cant access other users show page' do
+        get :show, params: { id: @user2.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
     end
 
-     context 'when a user is not logged in' do
-       it 'redirects to login' do
-         get :show, params: { id: user.id }
-         expect(response).to redirect_to(new_user_session_path)
-       end
-     end
-  end
+      context 'when a user is not logged in' do
+        it 'redirects to login' do
+          get :show, params: { id: @user.id }
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
 
+  end
 end
