6c9be10dd2eeb2cbef50d4929bd34e696aa456ad
frantzeska <saiti.frantzeska@gmail.com>
2019-03-29 21:21:34 +0200

    amend ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, :all    # permissions for every user, even if not logged in
+    #can :read, :all    # permissions for every user, even if not logged in
     if user.nil?
       user ||= User.new
     elsif
