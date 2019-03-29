89f918eae3e713cd8816d01c38f2bbd53dc23971
frantzeska <saiti.frantzeska@gmail.com>
2019-03-29 19:31:15 +0200

    try to fix the problem with functial test in application

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,7 @@ class Ability
     else
       #user ||= User.new # guest user (not logged in)
       can :manage, User, id: user.id
-      can [:show, :index], Order, user_id: user.id
+      #can [:show, :index], Order, user_id: user.id
       can :create, Comment, user_id: user.id
       can [:show, :index], Product
     # Define abilities for the passed in user here. For example:
