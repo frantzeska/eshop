bb2eaadf80707c07c4fa4260131c40294dc26f58
frantzeska <saiti.frantzeska@gmail.com>
2019-03-29 22:53:47 +0200

    try to add factories

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  #let(:user) { User.create!(email: 'email1@example.com', password: 'password1') }
-  #let(:user2) { User.create!(email: 'emails2@example.com', password: 'password2') }
+  let(:user) { User.create!(email: 'email1@example.com', password: 'password1') }
+  let(:user2) { User.create!(email: 'emails2@example.com', password: 'password2') }
 
-    @user = FactoryBot.create(:user)
-    @user2 = FactoryBot.create(:user)
-  
+    #@user = FactoryBot.create(:user)
+    #@user2 = FactoryBot.create(:user)
+
   describe 'GET #show' do
      context 'when a user is logged in' do
        before do
-        sign_in @user
+        sign_in user
       end
 
       it 'loads correct user details' do
-         get :show, params: {id: @user.id}
+         get :show, params: {id: user.id}
          expect(response).to be_ok
-         expect(assigns(:user)).to eq @user
+         expect(assigns(:user)).to eq user
        end
 
        it "it can't access the Show page of the second user" do
-         get :show, params: {id: @user2.id}
+         get :show, params: {id: user2.id}
          expect(response).to have_http_status(302)
          expect(response).to redirect_to(root_path)
        end
@@ -29,7 +29,7 @@ describe UsersController, type: :controller do
 
      context 'when a user is not logged in' do
        it 'redirects to login' do
-         get :show, params: { id: @user.id }
+         get :show, params: { id: user.id }
          expect(response).to redirect_to(new_user_session_path)
        end
      end
