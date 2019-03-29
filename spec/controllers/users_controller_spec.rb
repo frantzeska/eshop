5a078d5dffbe73543b6c617ca24c154656f889b4
frantzeska <saiti.frantzeska@gmail.com>
2019-03-29 22:13:22 +0200

    try to add factories

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,17 +10,17 @@ describe UsersController, type: :controller do
   describe 'GET #show' do
      context 'when a user is logged in' do
        before do
-        sign_in @user
+        sign_in user
       end
 
       it 'loads correct user details' do
-         get :show, params: {id: @user.id}
+         get :show, params: {id: user.id}
          expect(response).to be_ok
-         expect(assigns(:user)).to eq @user
+         expect(assigns(:user)).to eq user
        end
 
        it "it can't access the Show page of the second user" do
-         get :show, params: {id: @user2.id}
+         get :show, params: {id: user2.id}
          expect(response).to have_http_status(302)
          expect(response).to redirect_to(root_path)
        end
@@ -29,7 +29,7 @@ describe UsersController, type: :controller do
 
      context 'when a user is not logged in' do
        it 'redirects to login' do
-         get :show, params: { id: @user.id }
+         get :show, params: { id: user.id }
          expect(response).to redirect_to(new_user_session_path)
        end
      end
