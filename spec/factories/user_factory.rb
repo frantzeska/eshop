61d2c791acc49870edcc341654d2d1426bde456d
frantzeska <saiti.frantzeska@gmail.com>
2019-03-29 22:26:43 +0200

    try to add factories

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,9 +1,19 @@
 FactoryBot.define do
+
   factory :user do
     email "peter@example.com"
     password "1234567890"
     first_name "Peter"
-    last_name "Example"
+    last_name "Smith"
     admin false
   end
+
+  factory :admin, class: User do
+  email
+  password "password"
+  admin true
+  first_name "Admin"
+  last_name "User"
+  end
+
 end
