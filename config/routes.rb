4f25ae515c1f6d27957a4450383785e092b865b8
frantzeska <saiti.frantzeska@gmail.com>
2019-04-03 00:44:18 +0300

    6.6 create payments

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   post 'simple_pages/thank_you'
   post 'payments/create'
   root 'simple_pages#landing_page'
+
+  get 'products/index'
+  get 'products/new'
+  get 'products/edit'
+  get 'products/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :orders, only: [:index, :show, :create, :destroy]
