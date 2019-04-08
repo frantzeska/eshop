0d84b8aefc0f2acb09e50384e583081c8b5aeeac
frantzeska <saiti.frantzeska@gmail.com>
2019-04-08 20:51:35 +0300

    6.9 JavaScript on Rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   extend FriendlyId
-  friendly_id :name, use: :slugged
+  friendly_id :first_name, use: :slugged
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
