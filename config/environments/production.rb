0903aee704d98c8f028c5ba307b51dc8233aa8db
frantzeska <saiti.frantzeska@gmail.com>
2019-04-20 16:03:20 +0300

    6.13 real time updates with ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://bookstoreathens.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://bookstoreathens.herokuapp.com', 'http://bookstoreathens.herokuapp.com']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :orders, only: [:index, :show, :create, :destroy]
+  
+  mount ActionCable.server => '/cable'
 end
