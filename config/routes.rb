d585cfb3e6034828d84538a40937c65663ccee31
frantzeska <saiti.frantzeska@gmail.com>
2019-04-22 21:26:47 +0300

    6.14 Active Job & Advanced ActionCable

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
-  get '/products', to: 'products#index'
+  get 'products', to: 'products#index'
   post 'simple_pages/thank_you'
   post 'payments/create'
   root 'simple_pages#landing_page'
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :orders, only: [:index, :show, :create, :destroy]
-  
+
   mount ActionCable.server => '/cable'
 end
